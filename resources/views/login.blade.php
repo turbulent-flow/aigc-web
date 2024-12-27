d1170758579b64f073dfa8b76cccfaee96332c8e
liam <hello@mengliu.dev>
2024-12-27 22:28:09 +0800

    feat: echo error for login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,6 +7,16 @@
 <form action="{{ route('login') }}" method="post">
     @csrf
 
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
     <div>
         <label for="username">Username</label>
         <input name="username" type="text" value="{{ old('username') }}" />
